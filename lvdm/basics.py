245fb5600d46a24abe95201ac26a81b6da94edc4
xpanpanda <140177939+xpanpanda@users.noreply.github.com>
2024-03-23 11:02:51 +0800

    Update basics.py

--- lvdm/basics.py
+++ lvdm/basics.py
@@ -14,19 +14,28 @@ from utils.utils import instantiate_from_config
 def disabled_train(self, mode=True):
     """Overwrite model.train with this function to make sure train/eval mode
     does not change anymore."""
+    """这块代码的目标是为了强制关闭 PyTorch 模型的 .train() 方法，这样模型无论何时都会处于评估（evaluation）模式。
+    也就是说，model.train() 不会切换模型到训练模式，会保持它在评估模式"""
     return self
 
 def zero_module(module):
     """
     Zero out the parameters of a module and return it.
     """
+    """
+     PyTorch 模型（或者模型的一个模块）的所有参数设为零
+    """
     for p in module.parameters():
+        # 忽略前面计算结果，原地替换所有参数值为0
         p.detach().zero_()
     return module
 
 def scale_module(module, scale):
     """
     Scale the parameters of a module and return it.
+    """
+    """
+     PyTorch 模型（或者模型的一个模块）的所有参数都乘以一个新的倍数
     """
     for p in module.parameters():
         p.detach().mul_(scale)
@@ -67,6 +76,7 @@ def avg_pool_nd(dims, *args, **kwargs):
 
 
 def nonlinearity(type='silu'):
+    # 两种激活函数可以选择
     if type == 'silu':
         return nn.SiLU()
     elif type == 'leaky_relu':
@@ -74,7 +84,9 @@ def nonlinearity(type='silu'):
 
 
 class GroupNormSpecific(nn.GroupNorm):
+    # 组归一化->通道层面的归一化
     def forward(self, x):
+        # 在forward前将数据类型转化为float
         return super().forward(x.float()).type(x.dtype)
 
 
@@ -84,6 +96,7 @@ def normalization(channels, num_groups=32):
     :param channels: number of input channels.
     :return: an nn.Module for normalization.
     """
+    # 组归一化的normalization
     return GroupNormSpecific(num_groups, channels)
 
 
