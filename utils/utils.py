b5de437aea33826be8dfb831ce0976e7206ad2b8
xpanpanda <140177939+xpanpanda@users.noreply.github.com>
2024-03-23 11:41:32 +0800

    Update utils.py

--- utils/utils.py
+++ utils/utils.py
@@ -6,6 +6,10 @@ import torch.distributed as dist
 
 
 def count_params(model, verbose=False):
+    """
+    返回params中的参数总数
+    params架构：params_groups>model_params_group>params
+    """
     total_params = sum(p.numel() for p in model.parameters())
     if verbose:
         print(f"{model.__class__.__name__} has {total_params*1.e-6:.2f} M params.")
@@ -25,6 +29,9 @@ def check_istarget(name, para_list):
 
 
 def instantiate_from_config(config):
+    """
+    config是键值对,target为一个键，其值应该是一个字符串写着 module.cls
+    """
     if not "target" in config:
         if config == '__is_first_stage__':
             return None
@@ -37,8 +44,10 @@ def instantiate_from_config(config):
 def get_obj_from_str(string, reload=False):
     module, cls = string.rsplit(".", 1)
     if reload:
-        module_imp = importlib.import_module(module)
-        importlib.reload(module_imp)
+        """这两行代码通常用于开发时动态地加载或更新模块，以便快速地看到代码变化的效果，而无需重启 Python 解释器"""
+        module_imp = importlib.import_module(module) # 根据名称导入模块赋值给module_imp
+        importlib.reload(module_imp) # 重新在内存中加载
+    # 相当于from module import cls
     return getattr(importlib.import_module(module, package=None), cls)
 
 
